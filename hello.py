8ff2dee302a71eb7fd32d1c2786105d988ff076b
leo-edc <290071582@qq.com>
2019-01-10 10:01:49 +0800

    'aaa'

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ urls = (
 
 class index:
     def GET(self):
-        return "Hello, world!123"
+        return "Hello, world!1234"
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
